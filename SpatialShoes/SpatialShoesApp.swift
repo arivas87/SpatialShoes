56e9840f17e7cc31abc68ea1b30148c9c783a5c6
Arturo Rivas <arturo.rivas@icloud.com>
2024-08-08 14:32:33 +0200

    Initial Commit

---
+++ SpatialShoes/SpatialShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialShoesApp.swift
+//  SpatialShoes
+//
+//  Created by Arturo Rivas Arias on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpatialShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
